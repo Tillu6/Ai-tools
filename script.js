4a4b6a71c52ae6ef281ccbcff92df5fd63d4e90b
tillu6 <sakethreddy018@gmail.com>
2024-10-25 01:22:35 +1100

    Initial commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const websites = [
-    { name: "Google", url: "https://www.google.com" },
-    { name: "GitHub", url: "https://www.github.com" },
-    { name: "Stack Overflow", url: "https://stackoverflow.com" },
-    { name: "MDN Web Docs", url: "https://developer.mozilla.org" }
+    { name: "Google", url: "https://www.krea.ai/", subtype: "image or video generation" },
+    { name: "stable-diffusion", url: "https://huggingface.co/spaces/stabilityai/stable-diffusion", subtype: "image generation" },
+    { name: "reame.so", url: "https://readme.so/editor", subtype: "readme generator" },
+    
 ];
 
 const websiteList = document.getElementById('website-list');
@@ -10,6 +10,9 @@ const websiteList = document.getElementById('website-list');
 websites.forEach(website => {
     const listItem = document.createElement('li');
     listItem.className = 'list-group-item';
-    listItem.innerHTML = `<a href="${website.url}" target="_blank">${website.name}</a>`;
+    listItem.innerHTML = `
+        <a href="${website.url}" target="_blank">${website.name}</a>
+        <span class="badge badge-secondary ml-2">${website.subtype}</span>
+    `;
     websiteList.appendChild(listItem);
 });
